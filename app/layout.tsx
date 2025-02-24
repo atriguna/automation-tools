40582f1a817cf33f8686780a424edf4b48e49795
Ario Triguna <ario.triguna@kitabisa.com>
2025-02-24 15:03:22 +0700

    add anilitic vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100">{children}</body>
+      <body className="bg-gray-100">{children}<Analytics /></body>
     </html>
   );
 }
